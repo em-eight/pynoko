1c10d364972407c0b25d83acb26fdf53ab99a224
em-eight <m8em.eightm8@gmail.com>
2024-12-21 23:04:37 -0600

    update example

--- example.py
+++ example.py
@@ -1,15 +1,18 @@
 import pynoko
 
-mkw = pynoko.KWhatever()
+mkw = pynoko.KHostSystem()
 mkw.configure(pynoko.Course.Luigi_Circuit, pynoko.Character.Funky_Kong, pynoko.Vehicle.Flame_Runner, False)
 mkw.init()
 print("MKW initialized")
 buttons = pynoko.buttonInput([pynoko.ACCELERATE])
 print(f"buttons: {buttons}")
-mkw.setInput(buttons, 0, 0, pynoko.Trick.NoTrick)
-print("MKW set input")
-mkw.calc()
-print("MKW frame stepped")
-print(mkw.kartObjectProxy())
+
+for i in range(30000):
+    mkw.setInput(buttons, 7, 0, pynoko.Trick.NoTrick)
+    #print("MKW set input")
+    mkw.calc()
+    #print("MKW frame stepped")
+    print(mkw.kartObjectProxy())
+    print(mkw.raceCompletion())
 mkw.reset()
 print("MKW race reset")
